bec17dc2d95ac22209a11e876aa02aee7dce3d96
Rafael Valverde <omit.mv@gmail.com>
2025-08-26 10:56:19 -0300

    Configurações...

---
+++ src/main/java/com/transfuturo/config/security/JwtConfig.java
@@ -0,0 +1,24 @@
+package com.transfuturo.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private long expiration;
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    public String getSecret() {
+        return secret;
+    }
+    public long getExpiration() {
+        return expiration;
+    }
+    public String getIssuer() {
+        return issuer;
+    }
+}
